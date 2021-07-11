797a6a88ec24f2ca813672c6278a09395569f30e
Noah <noah@n0ahmc.com>
2021-07-10 19:39:54 -0500

    Added Custom Logger

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,5 @@
-module.exports = {
-  name: "ready",
-  once: true,
-  execute(client) {
-    console.log(`Ready! Logged in as ${client.user.tag}`);
-  },
+module.exports = async (client) => {
+  client.logger.ready(
+    `${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`
+  );
 };
--- index.js
+++ index.js
@@ -3,28 +3,30 @@ const Discord = require("discord.js");
 require("dotenv").config();
 
 const client = new Discord.Client();
+client.logger = require("./modules/Logger");
 client.commands = new Discord.Collection();
 
 const commandFiles = fs
   .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));
+client.logger.log(`Loading a total of ${commandFiles.length} commands.`);
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
+  client.logger.log(`Loading Command: ${command.name}`);
 }
 
 const eventFiles = fs
   .readdirSync("./events")
   .filter((file) => file.endsWith(".js"));
+client.logger.log(`Loading a total of ${eventFiles.length} events.`);
 
 for (const file of eventFiles) {
+  const eventName = file.split(".")[0];
+  client.logger.log(`Loading Event: ${eventName}`);
   const event = require(`./events/${file}`);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args, client));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args, client));
-  }
+  client.on(eventName, event.bind(null, client));
 }
 
 client.login(process.env.TOKEN);
