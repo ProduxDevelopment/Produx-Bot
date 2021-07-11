21b7a34b971297768230f8c80cc5a743918e0cbd
Noah <noah@n0ahmc.com>
2021-07-10 23:23:49 -0500

    Added normal and embed say command

---
+++ commands/say.js
@@ -0,0 +1,26 @@
+module.exports = {
+  name: "say",
+  args: true,
+  permissions: ["Team"],
+  usage: "[normal/embed] [your text/embed code]",
+  async execute(client, message, args) {
+    if (args[0].toLowerCase() === "normal") {
+      message.delete()
+      return message.channel.send(args.slice(1).join(' '));
+    } 
+    if (args[0].toLowerCase() === "embed") {
+      try {
+        JSON.parse(args.slice(1).join(' '))
+      } catch (e) {
+        return message.reply("Something went wrong!\nPlease Generate your embed with https://embedbuilder.nadekobot.me/")
+      }
+      message.delete()
+      const embed = JSON.parse(args.slice(1).join(' '))
+      return message.channel.send(embed.plainText, {
+      embed: embed
+    })
+    }
+    return message.reply('Invalid Usage')
+  
+  },
+};
