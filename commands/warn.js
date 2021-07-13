56de4386d0459599a71e4d7a4cf571c232dfc8fd
Noah <noah@n0ahmc.com>
2021-07-13 01:55:57 -0500

    Added Warn Command

---
+++ commands/warn.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "warn",
+  args: true,
+  permissions: ["Team"],
+  usage: "<@user/user id> [reason]",
+  async execute(client, message, args) {
+    let user =
+      message.mentions.users.first() ||
+      message.guild.members.cache.get(args[0]);
+    if (!user)
+      return message.reply(
+        "Invalid Usage\nProper Usage: `.warn <@user/user id> [reason]`"
+      );
+    await args.shift();
+    client.utils.warn(client, user, args.join(" "), message.author);
+    message.channel.send({
+      embed: {
+        color: "00ff00",
+        title: "User Warned!",
+        description: `You have successfully warned ${user}.`,
+      },
+    });
+  },
+};
