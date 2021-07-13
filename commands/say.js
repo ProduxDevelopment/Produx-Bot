b8f87a44f8561cfed82212e61d2aa7512088098e
Noah <noah@n0ahmc.com>
2021-07-13 01:53:21 -0500

    Ran Prettier

--- commands/say.js
+++ commands/say.js
@@ -5,22 +5,23 @@ module.exports = {
   usage: "<normal/embed> <your text/embed code>",
   async execute(client, message, args) {
     if (args[0].toLowerCase() === "normal") {
-      message.delete()
-      return message.channel.send(args.slice(1).join(' '));
-    } 
+      message.delete();
+      return message.channel.send(args.slice(1).join(" "));
+    }
     if (args[0].toLowerCase() === "embed") {
       try {
-        JSON.parse(args.slice(1).join(' '))
+        JSON.parse(args.slice(1).join(" "));
       } catch (e) {
-        return message.reply("Something went wrong!\nPlease Generate your embed with https://embedbuilder.nadekobot.me/")
+        return message.reply(
+          "Something went wrong!\nPlease Generate your embed with https://embedbuilder.nadekobot.me/"
+        );
       }
-      message.delete()
-      const embed = JSON.parse(args.slice(1).join(' '))
+      message.delete();
+      const embed = JSON.parse(args.slice(1).join(" "));
       return message.channel.send(embed.plainText, {
-      embed: embed
-    })
+        embed: embed,
+      });
     }
-    return message.reply('Invalid Usage')
-  
+    return message.reply("Invalid Usage");
   },
 };
