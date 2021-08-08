5f54137ccff20c759237c7110e23704ffdbc2a9d
Noah <noah@n0ahmc.com>
2021-08-08 18:56:04 -0500

    added slash command handler

--- events/ready.js
+++ events/ready.js
@@ -2,4 +2,13 @@ module.exports = async (client) => {
   client.logger.ready(
     `${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`
   );
+  try {
+    console.log("Started refreshing application (/) commands.");
+    const commands = client.commands.map(({ execute, ...data }) => data);
+    await client.guilds.cache.get("863559556416143361")?.commands.set(commands);
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
 };
