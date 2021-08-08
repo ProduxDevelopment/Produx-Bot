0994ccaa65c9c5f76804a0073aea10347d466861
Noah <noah@n0ahmc.com>
2021-08-08 18:56:15 -0500

    renamed file

---
+++ events/interactionCreate.js
@@ -0,0 +1,19 @@
+module.exports = async (client, interaction) => {
+  // console.log(interaction);
+  if (!interaction.isCommand()) return;
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    client.logger.cmd(
+      `[CMD] ${interaction.user.username} (${interaction.user.id}) ran command ${command.name}`
+    );
+    await command.execute(interaction);
+  } catch (error) {
+    client.logger.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+};
