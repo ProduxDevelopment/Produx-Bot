b6a662801ed7ec917e1db41de41ad5ce46c59d9f
Noah <noah@n0ahmc.com>
2021-08-08 22:03:27 -0500

    updated logging

--- events/ready.js
+++ events/ready.js
@@ -3,11 +3,10 @@ module.exports = async (client) => {
     `${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`
   );
   try {
-    console.log("Started refreshing application (/) commands.");
     const commands = client.commands.map(({ execute, ...data }) => data);
     await client.guilds.cache.get("863559556416143361")?.commands.set(commands);
 
-    console.log("Successfully reloaded application (/) commands.");
+    client.logger.log("Slash commands have been loaded.");
   } catch (error) {
     console.error(error);
   }
