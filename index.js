fe466b3a4b69d010776af81750c5967c088fee6a
Noah <noah@n0ahmc.com>
2021-08-08 22:03:16 -0500

    Cleaned console logs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.clear();
 const fs = require("fs");
 const { Client, Collection, Intents } = require("discord.js");
 require("dotenv").config();
@@ -24,28 +25,39 @@ const client = new Client({
 client.logger = require("./modules/Logger");
 client.utils = require("./modules/Utils");
 client.commands = new Collection();
-
 const commandFiles = fs
   .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));
-client.logger.log(`Loading a total of ${commandFiles.length} commands.`);
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
-  client.logger.log(`Loading Command: ${command.name}`);
+  //client.logger.log(`Loading Command: ${command.name}`);
+}
+client.logger.log(`${commandFiles.length} commands have been loaded.`);
+
+if (!fs.existsSync("db.json")) {
+  try {
+    client.logger.warn("db.json does not exist, making it now.");
+    fs.writeFileSync("db.json", JSON.stringify({}));
+  } catch (err) {
+    client.logger.error("Something went wrong while making db.json");
+    console.error(err);
+  }
+} else {
+  client.logger.log("db.json ready.");
 }
 
 const eventFiles = fs
   .readdirSync("./events")
   .filter((file) => file.endsWith(".js"));
-client.logger.log(`Loading a total of ${eventFiles.length} events.`);
 
 for (const file of eventFiles) {
   const eventName = file.split(".")[0];
-  client.logger.log(`Loading Event: ${eventName}`);
+  //client.logger.log(`Loading Event: ${eventName}`);
   const event = require(`./events/${file}`);
   client.on(eventName, event.bind(null, client));
 }
+client.logger.log(`${eventFiles.length} events have been loaded.`);
 
 client.login(process.env.TOKEN);
