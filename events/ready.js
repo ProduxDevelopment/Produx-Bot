5c44a58679c0c2d3fa83a6226525821d60fa03da
Noah <noah@n0ahmc.com>
2021-08-08 22:06:20 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = async (client) => {
     const commands = client.commands.map(({ execute, ...data }) => data);
     await client.guilds.cache.get("863559556416143361")?.commands.set(commands);
 
-    client.logger.log("Slash commands have been loaded.");
+    client.logger.log("Slash commands have been updated.");
   } catch (error) {
     console.error(error);
   }
